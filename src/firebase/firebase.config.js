cf300682986404dd656e7e3e83da60728946b608
PrinceTD <prince.talukder001@gmail.com>
2024-10-15 11:38:52 +0600

    added Auth

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB7DSr2nx2yHk_WZmOpQr2km8yUox4Dp0Y",
+  authDomain: "hopehues-45b1e.firebaseapp.com",
+  projectId: "hopehues-45b1e",
+  storageBucket: "hopehues-45b1e.appspot.com",
+  messagingSenderId: "768600090598",
+  appId: "1:768600090598:web:972833aa093996b37a1827"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
